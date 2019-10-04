22a57e71519be7524f0df7543c0ac07fc5b33585
shine-he <1151079644.com>
2019-10-04 22:51:47 +0800

    完善了一些代码，不仅比较了s指针和p指针的字符是否匹配，还添加了s指针当前位与前一位的比较以及p指针当前位与前一位的比较，代码已经可以运行5个用例，但规范还没修改。

--- textsti.cpp
+++ textsti.cpp
@@ -13,7 +13,7 @@ int main()
 }
 char compare(char *a,char *b)
 {
-    if (*a == *b || *b == '.')    //比较当前位置s、p的字符是否匹配
+    if (*a == *b || *b == '.')    //比较s、p第一位的字符是否匹配
     {
         b++;
         a++;
@@ -28,49 +28,44 @@ char compare(char *a,char *b)
             return 1;
         }
     }
-    while(*a!='\0')
+    if (*b != '\0')  
     {
-        if (*a == *(a-1))
+        while(*a!='\0')
         {
-            if (*b == '*');
-            else
-            {
-                if (*a == *b || *b == '.')    //比较当前位置s、p的字符是否匹配
-                {
-                    b++;
-                    a++;
-                }    
-                else
-                {
-                    if (*(b+1) == '*')
-                    b++;
-                    else
-                    {
-                        printf("false");
-                        return 1;
-                    }
-                }
-            }
-        }
-        else
-        {
-            if (*a == *b || *b == '.')    //比较当前位置s、p的字符是否匹配
+            if (*a == *b || *b == '.')      //比较当前位置s、p的字符是否匹配，匹配则s、p指针移下一位
             {
                 b++;
                 a++;
             }    
-            else
+            else if (*b == '*')             //p为*
             {
-                if (*(b+1) == '*')
-                b++;
-                else
+                if (*a == *(a-1))           //判断s当前位是否与上一位相等，相等则指针移下一位
+                a++;
+                else                        //不等则判断p上一位是否为.，是则s移位否则p移位
                 {
-                    printf("false");
-                    return 1;
+                    if (*(b-1) == '.')
+                    a++;
+                    else
+                    b++;
                 }
             }
+            else                            //p为字母或\0
+            {
+                printf("false");
+                return 1;
+            }
         }
-        a++;
+        if (*a == '\0' && *b!='\0') 
+        {
+            printf("false");
+            return 1;
+        }
+        else;
+    }
+    else
+    {
+        printf("false");
+        return 1;
     }
     
     printf("ture");
