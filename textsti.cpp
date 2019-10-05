4eef5d43f76e76e6b456d0df540c63556fccd8f1
shine-he <1151079644.com>
2019-10-05 23:28:19 +0800

    在之前的基础上添加了反向匹配比较，在反向匹配比较函数里添加了一个递归函数，正向比较和反向比较只要有一个返回值为一，则满足正则匹配。

--- textsti.cpp
+++ textsti.cpp
@@ -2,79 +2,123 @@
 #include<string.h>
 #include<stdbool.h>
 char compare (char *a,char *b);
+char recompare (char *a,char *b);
+char recur (char *k);
 bool isMatch(char *s,char *p);
-int main()
-{
+int main(){
+    char inter_varia;
+    int m,n,i,re1,re2;
     char s[20],p[20];
     printf("please input two strings s and p\n");
     printf("s:");
     gets(s);
     printf("p:");
     gets(p);
-    compare (s,p);
+    re1=compare (s,p);
+    m=strlen(s);
+    n=strlen(p);
+    for ( i = 0;i <= (m-1-i);i++)
+    {
+        inter_varia=*(s+i);
+        *(s+i)=*(s+m-1-i);
+        *(s+m-1-i)=inter_varia;
+    }
+    for ( i = 0;i <= (n-1-i);i++)
+    {
+        inter_varia=*(p+i);
+        *(p+i)=*(p+n-1-i);
+        *(p+n-1-i)=inter_varia;
+    }
+    re2 = recompare (s,p);
+
+    if (re1 == 1 || re2 == 1) {
+        printf("true");
+    }
+    else {
+        printf("false");
+    }
+    
 }
 char compare(char *a,char *b)
 {
-    if (*a == *b || *b == '.')    //比较s、p第一位的字符是否匹配
-    {
+    if (*a == *b || *b == '.') {
         b++;
         a++;
-    }    
-    else
-    {
-        if (*(b+1) == '*')
-        b++;
-        else
-        {
-            printf("false");
-            return 1;
-        }
     }
-    if (*b != '\0')  
-    {
+    else {
+        if (*(b+1) == '*')  b++;
+        else    return 0;
+    }
+
+    if (*b != '\0' && *a != '\0') {
         while(*a!='\0')
         {
-            if (*a == *b || *b == '.')      //比较当前位置s、p的字符是否匹配，匹配则s、p指针移下一位
-            {
+            if (*a == *b || *b == '.') {
                 b++;
                 a++;
             }    
-            else if (*b == '*')             //p为*
-            {
-                if (*a == *(a-1))           //判断s当前位是否与上一位相等，相等则指针移下一位
-                a++;
-                else                        //不等则判断p上一位是否为.，是则s移位否则p移位
-                {
-                    if (*(b-1) == '.')
+            else if (*b == '*') {
+                if (*a == *(a-1)) {          
                     a++;
-                    else
-                    b++;
+                }
+                else {
+                    if (*(b-1) == '.')  a++;
+                    else    b++;
                 }
             }
-            else                            //p为字母或\0
-            {
-                printf("false");
-                return 1;
+            else {
+                return 0;
             }
         }
-       /*
-        if (*a == '\0' && *b!='\0') 
-        {
-            printf("false");
-            return 1;
-        }
-        else;
-        */
     }
-    else
-    {
-        printf("false");
-        return 1;
+    else {
+
+        return 0;
     }
-    
-    printf("ture");
     return 1;
 }
-bool isMatch(char *s,char *p){
+char recompare(char *a,char *b)
+{
+    if (*a == *b || *b == '.') {
+        b++;
+        a++;
+    }    
+    else if(*b == '*') {
+        if (*(b+1) == *a)   a++;
+        else    return 0;
+    }
+    else{}
 
+    if (*b != '\0' && *a!= '\0') {
+        while(*a!='\0' ) {
+            if (*a == *b || *b == '.') {
+                b++;
+                a++;
+            }    
+            else if (*b == '*') {
+                if (*a == *(a+1))   a++;
+                else    b++;
+            }
+            else {
+                return 0;
+            }
+        }
+        return recur(b);
+    }
+    else {
+        return 0;
+    }
 }
+char recur(char *k){
+    if(*k == '*')
+    {
+        k++;
+        while(*k != '\0' && *(k+1) != '\0') {
+            if (*k == *(k+1))  k++;
+            else   return recur(k);
+        }
+        return 1;
+    }
+    else return 0;
+}
+bool isMatch(char *s,char *p){}
